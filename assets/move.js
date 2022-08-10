1f1d77311102b4dfaf0cef2583ac26912bfb04ea
dgraham742 <106363652+dgraham742@users.noreply.github.com>
2022-08-10 17:07:53 -0500

    first commit

--- assets/move.js
+++ assets/move.js
@@ -1,1 +1,44 @@
-var timer=
+// Grab a reference to the "Start" button
+var startButton = document.getElementById("start");
+var questionsContainer = document.querySelector(".questions");
+var startContainer = document.querySelector(".start-game");
+
+var timer;
+
+function startTimer() {
+    // assign a start time
+    timer = 30;
+    console.log(timer)
+    var myfunc = setInterval(function() {
+        timer--;
+        console.log(timer)
+    },1000);
+}
+
+// var startButton = document.querySelector(".container");
+
+console.log(startButton);
+startButton.addEventListener("click", function(event) {
+    console.log("click");
+
+    // Start a Timer
+    startTimer();
+    // Show first question
+    showQuestion();
+});
+
+
+function showQuestion() {
+    console.log("Show question");
+
+    startContainer.setAttribute("class", "hide");
+    questionsContainer.setAttribute("class", "show");
+}
+
+function other() {
+
+}
+
+function check() {
+    // add or remove time
+}
