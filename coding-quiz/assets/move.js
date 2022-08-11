0114c0b0c193e2a8845b0d9dffd0c8c1a0f6ddf8
dgraham742 <106363652+dgraham742@users.noreply.github.com>
2022-08-10 20:34:45 -0500

    Add files via upload

---
+++ coding-quiz/assets/move.js
@@ -0,0 +1,52 @@
+// Grab a reference to the "Start" button
+var startButton = document.getElementById("start");
+var questionsContainer = document.querySelector(".questions");
+var startContainer = document.querySelector(".start-game");
+
+var timer = 30;
+
+function startTimer() {
+    // assign a start time
+
+    timer = 30;
+    console.log(timer)
+    var myfunc = setInterval(function () {
+        var timerDisplay = document.getElementById("timerDisplay")
+        if (timer > 0){
+        timer--;
+        debugger;
+        console.log(timer)
+        timerDisplay.textContent = `Time left:${timer}`
+    }   else {
+        clearInterval(myfunc)
+    }
+    }, 1000);
+}
+
+// var startButton = document.querySelector(".container");
+
+console.log(startButton);
+startButton.addEventListener("click", function (event) {
+    console.log("click");
+
+    // Start a Timer
+    startTimer();
+    // Show first question
+    showQuestion();
+});
+
+
+function showQuestion() {
+    console.log("Show question");
+
+    startContainer.setAttribute("class", "hide");
+    questionsContainer.setAttribute("class", "show");
+}
+
+function other() {
+
+}
+
+function check() {
+    // add or remove time
+}
